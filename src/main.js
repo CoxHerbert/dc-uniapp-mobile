4245d9e12170ad505c1dfa77c0b2be8fc3bba383
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2026-01-13 14:58:12 +0800

    Remove invalid Wot Design Uni style import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 import { WotDesign } from "wot-design-uni";
-import "wot-design-uni/dist/style.css";
 export function createApp() {
 	const app = createSSRApp(App);
 	app.use(WotDesign);
