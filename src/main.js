ad533db6a058f5ec230660114a9b451ebe461d5b
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2026-01-13 14:39:12 +0800

    Add Wot Design Uni initialization

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import {
-	createSSRApp
-} from "vue";
+import { createSSRApp } from "vue";
 import App from "./App.vue";
+import WotDesign from "wot-design-uni";
+import "wot-design-uni/dist/style.css";
 export function createApp() {
 	const app = createSSRApp(App);
+	app.use(WotDesign);
 	return {
 		app,
 	};
