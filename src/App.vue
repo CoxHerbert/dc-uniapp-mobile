71d2600d504d96d922486665bd0528d27aea520c
鲍泽楠 <coxherbert98@gmail.com>
2026-01-13 09:47:31 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
-<script>
-export default {
-  onLaunch: function () {
-    console.log('App Launch')
-  },
-  onShow: function () {
-    console.log('App Show')
-  },
-  onHide: function () {
-    console.log('App Hide')
-  },
-}
+<script setup lang="ts">
+onLaunch(() => {})
 </script>
 
-<style>
-/*每个页面公共css */
+<style lang="scss">
+.page-wraper {
+  min-height: calc(100vh - var(--window-top));
+  box-sizing: border-box;
+  background: #f9f9f9;
+}
+
+.wot-theme-dark.page-wraper {
+  background: #222;
+}
 </style>
