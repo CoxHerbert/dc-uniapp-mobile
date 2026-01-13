dfb6d836ff58715d528cab939bef100463afc8c8
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2026-01-13 14:15:01 +0800

    Initialize uni-app with Wot Design

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <view class="app">
+    <slot />
+  </view>
+</template>
+
+<style lang="scss">
+@import "wot-design-uni/dist/style.css";
+
+.app {
+  min-height: 100vh;
+  background-color: #f7f8fa;
+}
+</style>
